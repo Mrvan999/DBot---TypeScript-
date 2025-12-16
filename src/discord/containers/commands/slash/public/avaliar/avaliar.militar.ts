9a8771e6a3d445a63ae722026b2a932a69e86604
YuriGRG <134341981+YuriGRG@users.noreply.github.com>
2025-12-16 19:02:26 -0300

    feat: Avaliar Militares/OPM

--- src/discord/containers/commands/slash/public/avaliar/avaliar.militar.ts
+++ src/discord/containers/commands/slash/public/avaliar/avaliar.militar.ts
@@ -1,13 +1,14 @@
 import { brBuilder, createContainer, createTextDisplay, Separator } from "@magicyan/discord";
+import { GuildMember } from "discord.js";
 import { icon } from "../../../../../../functions/utils/emojis.js";
 
-export function avaliarMilitarContainer(militar: string, estrelas: string, avaliacao: string) {
+export function avaliarMilitarContainer(militarMember: GuildMember, estrelas: string, avaliacao: string) {
     return createContainer({
         components: [
             createTextDisplay(
                 brBuilder(
                     `## ${icon.pmesp} Avaliação de Militar`,
-                    `${icon.user} ${militar}`,
+                    `${icon.user} ${militarMember.user}`,
                     `-# ${estrelas}`
                 )
             ),
